87e91ab48731b817c213cbcd540be66f9e760d5e
Naftoreiclag <naftoreiclag@gmail.com>
2014-04-23 19:11:16 -0700

    private-ified code

--- source/naftoreiclag/village/Main.java
+++ source/naftoreiclag/village/Main.java
@@ -13,8 +13,6 @@ import java.nio.FloatBuffer;
 
 import org.lwjgl.BufferUtils;
 import org.lwjgl.LWJGLException;
-import org.lwjgl.input.Keyboard;
-import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.util.glu.Sphere;
