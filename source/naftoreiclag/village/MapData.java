9c5406f3041e8cd55f4295568cc7d32d3f30a29a
Naftoreiclag <naftoreiclag@gmail.com>
2014-04-25 17:41:08 -0700

    you are so beautiful

--- source/naftoreiclag/village/MapData.java
+++ source/naftoreiclag/village/MapData.java
@@ -67,80 +67,6 @@ public class MapData
 		}
 	}
 	
-	public void makeGrassModel()
-	{
-		List<Float> vert = new ArrayList<Float>();
-		List<Integer> inde = new ArrayList<Integer>();
-		
-		List<Triangle> trias = new ArrayList<Triangle>();
-		
-		boolean[][][] included = new boolean[size - 1][size - 1][4];
-
-		for(int x = 0; x < size - 1; ++ x)
-		{
-			for(int z = 0; z < size - 1; ++ z)
-			{
-				// M  0  D
-				//
-				// 2  P  3
-				//
-				// B  1  N
-				
-				float m = map[x    ][z    ];
-				float d = map[x + 1][z    ];
-				float b = map[x    ][z + 1];
-				float n = map[x + 1][z + 1];
-				
-				double mbn = steepness(m, b, n);
-				double bnd = steepness(b, n, d);
-				double ndm = steepness(n, d, m);
-				double dmb = steepness(d, m, b);
-				/*
-				double flat = smallest(mbn, bnd, ndm, dmb);
-				
-				if(mbn == flat)
-				{
-					p = (m + n) / 2.0f;
-				}
-				else if(bnd == flat)
-				{
-					p = (b + d) / 2.0f;
-				}
-				else if(ndm == flat)
-				{
-					p = (n + m) / 2.0f;
-				}
-				else if(dmb == flat)
-				{
-					p = (d + b) / 2.0f;
-				}
-				*/
-				
-				double flatThresh = 0.5;
-				
-				included[x][z][0] = mbn < flatThresh;
-				included[x][z][1] = mbn < flatThresh;
-				included[x][z][2] = mbn < flatThresh;
-				included[x][z][3] = mbn < flatThresh;
-			}
-		}
-		
-		IntBuffer topIntBuff = BufferUtils.createIntBuffer(inde.size());
-		for(int f : inde)
-		{
-			topIntBuff.put(f);
-		}
-		
-		FloatBuffer topVertsBuff = BufferUtils.createFloatBuffer(vert.size());
-		for(float f : vert)
-		{
-			topVertsBuff.put(f);
-		}
-		
-		rock.putVerts(topVertsBuff);
-		rock.putIndices(topIntBuff, inde.size());
-	}
-	
 	public void forceUpdateNormals()
 	{
 		for(int x = 0; x < size; ++ x)
@@ -245,6 +171,8 @@ public class MapData
 				
 				double flat = smallest(mbn, bnd, ndm, dmb);
 				
+				boolean bothAreSteep = false;
+				
 				if(mbn == flat || ndm == flat)
 				{
 					if(mbn < steepThres)
@@ -333,6 +261,8 @@ public class MapData
 							// NDM is steep
 							
 							mb_rock.addTriangle(nV, dV, mV);
+							
+							bothAreSteep = true;
 						}
 					}
 				}
@@ -356,10 +286,10 @@ public class MapData
 							// BND is flat
 							// DMB is steep
 							
+							mb_rock.addTriangle(dV, mV, bV);
+							
 							float bndA = (b + n + d) / 3.0f;
 							float dmbA = (d + m + b) / 3.0f;
-							
-							mb_rock.addTriangle(dV, mV, bV);
 							
 							if(bndA > dmbA)
 							{
@@ -426,8 +356,76 @@ public class MapData
 							// DMB is steep
 							
 							mb_rock.addTriangle(dV, mV, bV);
+							
+							bothAreSteep = true;
 						}
 					}
+				}
+				
+				if(bothAreSteep)
+				{
+					// M     D
+					//
+					//    P   
+					//
+					// B     N
+					
+					if(z != 0)
+					{
+						double triA = steepness(map[x + 1][z], map[x][z], map[x][z - 1]);
+						double triB = steepness(map[x + 1][z], map[x][z], map[x + 1][z - 1]);
+						
+						if(triA < steepThres || triB < steepThres)
+						{
+							float dmA = (d + m) / 2.0f;
+							float bnA = (b + n) / 2.0f;
+							
+							if(dmA > bnA)
+							{
+								// hang
+								
+								Vector3f foo = new Vector3f(mx - dx, my - dy, mz - dz);
+								Vector3f jut = Vector3f.cross(mapNormals[dxi][dzi], foo, null);
+								jut.y *= 0.3f;
+								jut.normalise();
+								
+								mb_sidegrass.addQuad(
+										dx, dy, dz, mapNormals[dxi][dzi], 0.99f, 0.01f, 
+										mx, my, mz, mapNormals[mxi][mzi], 0.01f, 0.01f, 
+										mx + jut.x, my + jut.y, mz + jut.z, mapNormals[mxi][mzi], 0.01f, 0.99f, 
+										dx + jut.x, dy + jut.y, dz + jut.z, mapNormals[dxi][dzi], 0.99f, 0.99f);
+							}
+						}
+					}
+					/*
+					if(x != 0)
+					{
+						double triA = steepness(map[x][z], map[x][z + 1], map[x - 1][z]);
+						double triB = steepness(map[x][z], map[x][z + 1], map[x - 1][z + 1]);
+						
+						if(triA < steepThres || triB < steepThres)
+						{
+							float dmA = (d + m) / 2.0f;
+							float bnA = (b + n) / 2.0f;
+							
+							if(dmA > bnA)
+							{
+								// hang
+								
+								Vector3f foo = new Vector3f(mx - dx, my - dy, mz - dz);
+								Vector3f jut = Vector3f.cross(mapNormals[dxi][dzi], foo, null);
+								jut.y *= 0.3f;
+								jut.normalise();
+								
+								mb_sidegrass.addQuad(
+										dx, dy, dz, mapNormals[dxi][dzi], 0.99f, 0.01f, 
+										mx, my, mz, mapNormals[mxi][mzi], 0.01f, 0.01f, 
+										mx + jut.x, my + jut.y, mz + jut.z, mapNormals[mxi][mzi], 0.01f, 0.99f, 
+										dx + jut.x, dy + jut.y, dz + jut.z, mapNormals[dxi][dzi], 0.99f, 0.99f);
+							}
+						}
+					}
+					*/
 				}
 			}
 		}
@@ -437,163 +435,14 @@ public class MapData
 		sidegrass = mb_sidegrass.bake();
 	}
 	
+	private void performSteepSteepCheck(int x, int z, ModelBuilder mb_sidegrass)
+	{
+		
+	}
+
 	public Vector3f straighten(Vector3f a)
 	{
 		return (Vector3f) (new Vector3f(a.x, a.y * 2,a.z)).normalise();
-	}
-	
-	public void makeModelAlt()
-	{
-		ModelBuilder mb = new ModelBuilder();
-
-		for(int x = 0; x < size - 1; ++ x)
-		{
-			for(int z = 0; z < size - 1; ++ z)
-			{
-				// M  0  D
-				//
-				// 2  P  3
-				//
-				// B  1  N
-				
-				float m = map[x    ][z    ];
-				float d = map[x + 1][z    ];
-				float b = map[x    ][z + 1];
-				float n = map[x + 1][z + 1];
-				
-				mb.addQuad(
-						/* M */ (x    ) * horzu, m * vertu, (z    ) * horzu, new Vector3f(0.0f, 0.0f, 0.0f), x    , z,
-						/* B */ (x    ) * horzu, b * vertu, (z + 1) * horzu, new Vector3f(0.0f, 0.0f, 0.0f), x    , z + 1,
-						/* N */ (x + 1) * horzu, n * vertu, (z + 1) * horzu, new Vector3f(0.0f, 0.0f, 0.0f), x + 1, z + 1,
-						/* D */ (x + 1) * horzu, d * vertu, (z    ) * horzu, new Vector3f(0.0f, 0.0f, 0.0f), x + 1, z    );
-			}
-		}
-		
-		grass = mb.bake();
-	}
-	
-	public void makeModel()
-	{
-		// Size squared x floats per vertex x addition of middle vertex
-		FloatBuffer verts = BufferUtils.createFloatBuffer(size * size * 8 * 2);
-		
-		for(int x = 0; x < size; ++ x)
-		{
-			for(int z = 0; z < size; ++ z)
-			{
-				/*
-				 *     A                X ->
-				 *                    
-				 * C   M   D          Z
-				 *       P            |
-				 *     B   N          V
-				 *
-				 * M is the point defined by x, z
-				 */
-				
-				// Calculate height of these points ===
-				
-				float m = map[x][z];
-				float c = map[x > 0 ? x - 1 : x][z];
-				float d = map[x < size - 1 ? x + 1 : x][z];
-				float a = map[x][z > 0 ? z - 1 : z];
-				float b = map[x][z < size - 1 ? z + 1 : z];
-				float n = map[x < size - 1 ? x + 1 : x][z < size - 1 ? z + 1 : z];
-				float p = (m + n + d + b) / 4.0f;
-				
-				double mbn = steepness(m, b, n);
-				double bnd = steepness(b, n, d);
-				double ndm = steepness(n, d, m);
-				double dmb = steepness(d, m, b);
-				
-				double flat = smallest(mbn, bnd, ndm, dmb);
-				
-				if(mbn == flat)
-				{
-					p = (m + n) / 2.0f;
-				}
-				else if(bnd == flat)
-				{
-					p = (b + d) / 2.0f;
-				}
-				else if(ndm == flat)
-				{
-					p = (n + m) / 2.0f;
-				}
-				else if(dmb == flat)
-				{
-					p = (d + b) / 2.0f;
-				}
-				
-				
-				// Calculate normals for M ===
-				
-				float cd_d = c - d;
-				if(x == 0 || x == size - 1)
-				{
-					cd_d *= 2;
-				}
-				
-				float ab_d = a - b;
-				if(z == 0 || z == size - 1)
-				{
-					ab_d *= 2;
-				}
-
-				Vector3f m_n = new Vector3f(cd_d * vertu, 2 * horzu, ab_d * vertu);
-				m_n.normalise();
-
-				// Add M to data ===
-				
-				verts.put(x * horzu).put(m * vertu).put(z * horzu).put(m_n.x).put(m_n.y).put(m_n.z).put(x).put(z);
-				
-				// Calculate normals for P ===
-				
-				Vector3f m2d = new Vector3f(vertu, d - m, 0);
-				Vector3f m2b = new Vector3f(0, b - m, vertu);
-				Vector3f b2n = new Vector3f(vertu, n - b, 0);
-				Vector3f d2n = new Vector3f(0, n - d, vertu);
-
-				Vector3f p_n_1 = new Vector3f();
-				Vector3f p_n_2 = new Vector3f();
-				
-				Vector3f.cross(m2b, m2d, p_n_1);
-				Vector3f.cross(d2n, b2n, p_n_2);
-				
-				Vector3f p_n = new Vector3f();
-				Vector3f.add(p_n_1, p_n_2, p_n);
-				p_n.normalise();
-				
-
-				// Add P to data ===
-				
-				verts.put((x + 0.5f) * horzu).put(p * vertu).put((z + 0.5f) * horzu).put(p_n.x).put(p_n.y).put(p_n.z).put(x + 0.5f).put(z + 0.5f);
-			}
-		}
-		
-		grass.putVerts(verts);
-		
-		IntBuffer indices = BufferUtils.createIntBuffer(((size - 1) * (size - 1)) * 12);
-
-		for(int x = 0; x < size - 1; ++ x)
-		{
-			for(int z = 0; z < size - 1; ++ z)
-			{
-				// M   D
-				//   P
-				// B   N
-				
-				int mi = posToLin(x    , z    );
-				int di = posToLin(x + 1, z    );
-				int bi = posToLin(x    , z + 1);
-				int ni = posToLin(x + 1, z + 1);
-				int pi = mi + 1;
-				
-				indices.put(pi).put(mi).put(di).put(pi).put(di).put(ni).put(pi).put(ni).put(bi).put(pi).put(bi).put(mi);
-			}
-		}
-		
-		grass.putIndices(indices, ((size - 1) * (size - 1)) * 12);
 	}
 	
 	private double smallest(double a, double b, double c, double d)
@@ -606,6 +455,11 @@ public class MapData
 		float mean = (a + b + c) / 3.0f;
 		
 		return Math.abs(mean - a) + Math.abs(mean - b) + Math.abs(mean - c);
+	}
+	
+	private double steepness(float a, float b)
+	{
+		return Math.abs(a - b);
 	}
 	
 	private int posToLin(int x, int z)
