a9abf5767ea5e4bb92672920497c03a790fa5a13
Naftoreiclag <naftoreiclag@gmail.com>
2014-04-26 16:40:28 -0700

    something

--- source/naftoreiclag/village/rendering/ModelBuilder.java
+++ source/naftoreiclag/village/rendering/ModelBuilder.java
@@ -6,10 +6,8 @@
 
 package naftoreiclag.village.rendering;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.FloatBuffer;
@@ -194,70 +192,6 @@ public class ModelBuilder
 			fw.close();
 		}
 		catch (IOException e) { e.printStackTrace(); }
-	}
-	
-	public static Model fromObjFile(String filename) throws IOException
-	{
-		int vertCount = 0;
-		int indCount = 0;
-
-		String line;
-
-		BufferedReader read1 = new BufferedReader(new FileReader(new File(filename)));
-		while((line = read1.readLine()) != null)
-		{
-			String[] data = line.split(" ");
-			
-			if(data[0].equals("v"))
-			{
-				vertCount ++;
-			}
-			else if(data[0].equals("f"))
-			{
-				indCount += 3;
-			}
-		}
-		read1.close();
-
-		FloatBuffer verts = BufferUtils.createFloatBuffer(vertCount * 8);
-		IntBuffer indices = BufferUtils.createIntBuffer(indCount);
-		
-		int numNorms;
-		int numTexs;
-		
-		BufferedReader read2 = new BufferedReader(new FileReader(new File(filename)));
-		while((line = read2.readLine()) != null)
-		{
-			String[] data = line.split(" ");
-			
-			if(data[0].equals("#"))
-			{
-			    continue;
-			}
-			else if(data[0].equals("v"))
-			{
-				verts.put(Float.valueOf(data[1])).put(Float.valueOf(data[2])).put(Float.valueOf(data[3]));
-			}
-			else if(data[0].equals("vt"))
-			{
-				verts.put(Float.valueOf(data[1])).put(Float.valueOf(data[2])).put(Float.valueOf(data[3]));
-			}
-			else if(data[0].equals("vn"))
-			{
-				verts.put(Float.valueOf(data[1])).put(Float.valueOf(data[2])).put(Float.valueOf(data[3]));
-			}
-			else if(data[0].equals("f"))
-			{
-			}
-			else
-			{
-				System.err.println("Corrupt line in .obj! Skipping that line and hoping for best...");
-				continue;
-			}
-		}
-		read2.close();
-
-		return new Model(verts, indices, indCount, Model.WeaveType.nVnTnN);
 	}
 	
 	// Class for storing a single vertex's data
