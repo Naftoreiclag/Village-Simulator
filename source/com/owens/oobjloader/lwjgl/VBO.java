89dc667bdc100a0642f3d85b4d9f0f1e9e113415
Naftoreiclag <naftoreiclag@gmail.com>
2014-04-26 17:33:04 -0700

    refactoring

--- source/com/owens/oobjloader/lwjgl/VBO.java
+++ source/com/owens/oobjloader/lwjgl/VBO.java
@@ -5,17 +5,35 @@
 
 package com.owens.oobjloader.lwjgl;
 
+import static org.lwjgl.opengl.GL11.GL_FLOAT;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
+import static org.lwjgl.opengl.GL11.glBindTexture;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL11.glNormalPointer;
+import static org.lwjgl.opengl.GL11.glTexCoordPointer;
+import static org.lwjgl.opengl.GL11.glVertexPointer;
+import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.glBindBuffer;
+
+import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL15;
+
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.awt.*;
 import java.util.*;
+
 import org.lwjgl.BufferUtils;
 
 public class VBO
 {
+
 	public final static int FL_SIZE = 4;
 	public final static int INDICE_SIZE_BYTES = 4;
 	public final static int ATTR_V_FLOATS_PER = 3;
@@ -27,7 +45,6 @@ public class VBO
 	public final static int ATTR_V_OFFSET_FLOATS = 0;
 	public final static int ATTR_N_OFFSET_FLOATS = ATTR_V_FLOATS_PER;
 	public final static int ATTR_N_OFFSET_BYTES = ATTR_N_OFFSET_FLOATS * FL_SIZE;
-
 	public final static int ATTR_T_OFFSET_FLOATS = ATTR_V_FLOATS_PER + ATTR_N_FLOATS_PER;
 	public final static int ATTR_T_OFFSET_BYTES = ATTR_T_OFFSET_FLOATS * FL_SIZE;
 	public final static int ATTR_V_STRIDE2_BYTES = ATTR_SZ_FLOATS * FL_SIZE;
@@ -35,44 +52,28 @@ public class VBO
 	public final static int ATTR_T_STRIDE2_BYTES = ATTR_SZ_FLOATS * FL_SIZE;
 
 	private int textId = 0;
-	private int verticeAttributesID = 0; // Vertex Attributes VBO ID
-	private int indicesID = 0; // indice VBO ID
-	private int indicesCount = 0;
+	private int vertexHandle = 0; // Vertex Attributes VBO ID
+	private int indexHandle = 0; // indice VBO ID
+	private int numIndices = 0;
 
-	public VBO(int textId, int verticeAttributesID, int indicesID, int indicesCount)
+	public VBO(int textId, int verticeAttributesID, int indicesID,
+			int indicesCount)
 	{
 		this.textId = textId;
-		this.verticeAttributesID = verticeAttributesID;
-		this.indicesID = indicesID;
-		this.indicesCount = indicesCount;
+		this.vertexHandle = verticeAttributesID;
+		this.indexHandle = indicesID;
+		this.numIndices = indicesCount;
 	}
 
 	public void render()
 	{
-
-		GL11.glEnable(GL11.GL_TEXTURE_2D);
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, textId); // Bind The Texture
-
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, verticeAttributesID);
-
-		GL11.glEnableClientState(GL11.GL_VERTEX_ARRAY);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, textId);
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexHandle);
 		GL11.glVertexPointer(3, GL11.GL_FLOAT, ATTR_V_STRIDE2_BYTES, ATTR_V_OFFSET_BYTES);
-
-		GL11.glEnableClientState(GL11.GL_NORMAL_ARRAY);
 		GL11.glNormalPointer(GL11.GL_FLOAT, ATTR_N_STRIDE2_BYTES, ATTR_N_OFFSET_BYTES);
-
-		GL11.glEnableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
 		GL11.glTexCoordPointer(2, GL11.GL_FLOAT, ATTR_T_STRIDE2_BYTES, ATTR_T_OFFSET_BYTES);
-
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, indicesID);
-		GL11.glDrawElements(GL11.GL_TRIANGLES, indicesCount, GL11.GL_UNSIGNED_INT, 0);
-
-		/*
-		GL11.glDisableClientState(GL11.GL_VERTEX_ARRAY);
-		GL11.glDisableClientState(GL11.GL_NORMAL_ARRAY);
-		GL11.glDisableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
-		GL11.glDisable(GL11.GL_TEXTURE_2D);
-		*/
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, indexHandle);
+		GL11.glDrawElements(GL11.GL_TRIANGLES, numIndices, GL11.GL_UNSIGNED_INT, 0L);
 	}
 
 	public void destroy()
@@ -80,10 +81,10 @@ public class VBO
 		// NOTE: We don't delete the textureID
 		IntBuffer ib = BufferUtils.createIntBuffer(1);
 		ib.reset();
-		ib.put(verticeAttributesID);
+		ib.put(vertexHandle);
 		GL15.glDeleteBuffers(ib);
 		ib.reset();
-		ib.put(indicesID);
+		ib.put(indexHandle);
 		GL15.glDeleteBuffers(ib);
 	}
 }
